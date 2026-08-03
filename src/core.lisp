d8443a0a56bf5fa8fa6336437441b68687176450
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-03 04:06:57 -0400

    fix: make string library version reload-safe on SBCL

--- src/core.lisp
+++ src/core.lisp
@@ -1,6 +1,12 @@
 (in-package #:cl-discord-self)
 
-(defconstant +library-version+ "0.0.1-phase0")
+(defparameter +library-version+ "0.0.1-phase0"
+  "Public library version string.
+
+This is a parameter rather than a DEFCONSTANT because Common Lisp does not
+require separately evaluated string literals to be EQL. SBCL correctly rejects
+reloading a string DEFCONSTANT when the new string object is not EQL to the
+previous value.")
 
 (defun library-version ()
   +library-version+)
